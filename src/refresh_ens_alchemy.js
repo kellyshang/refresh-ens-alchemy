d1e9d9d50bbaf94a155d0717c7f391a949676f5f
kellyshang <yysk1122@126.com>
2022-08-23 17:51:44 +0800

    refactor: update Alchemy sdk

--- src/refresh_ens_alchemy.js
+++ src/refresh_ens_alchemy.js
@@ -1,11 +1,11 @@
-const { getNftsForOwner, refreshNftMetadata, initializeAlchemy } = require("@alch/alchemy-sdk");
-const alchemy = initializeAlchemy();
+const { Alchemy } = require("alchemy-sdk");
+const alchemy = new Alchemy();
 
 let owner
 const contractAddress = '0x57f1887a8bf19b14fc0df6fd9b2acc9af147ea85';
 
 const getENS = async () => {
-    const getENSResponse = await getNftsForOwner(alchemy, owner)
+    const getENSResponse = await alchemy.nft.getNftsForOwner(owner)
     const ENSTokenInfo = getENSResponse.ownedNfts.filter(item => item.contract.address == contractAddress)
     let ENStokenIDs = []
     ENSTokenInfo.forEach(e => {
@@ -21,7 +21,7 @@ async function refresh() {
     console.log(`ENStokenIDs length: ${tokenIds.length}`)
     for (let i = 0; i < tokenIds.length; i++) {
         console.log(`to refresh ens tokenID: ${tokenIds[i]}`)
-        const refreshresponse = await refreshNftMetadata(alchemy, contractAddress, tokenIds[i]);
+        const refreshresponse = await alchemy.nft.refreshNftMetadata(contractAddress, tokenIds[i]);
         console.log(`refreshresponse: ${refreshresponse}`)
     }
     console.log("after refreshed: ")
